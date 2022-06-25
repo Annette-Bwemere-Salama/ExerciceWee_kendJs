f79dfb5ebaaa2e416180718c587bab3fde55eaf4
annette BWEMERE <bwemereannette@gmail.com>
2022-06-25 10:45:10 +0200

    feat:Ajout et manipuation du tableau

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 // arrays-variable thah holf multiple values
 
-const fruits = 
+const fruits = ['apples', 'oranges', 'pers'];
+
+fruits[3] = 'grapes';
+fruits.push('mangos');
+fruits.unshift('strawberries');
+console.log(Array.isArray('hello'));
+console.log(fruits.indexOf('oranges'));
+
+console.log(fruits);
+
+//const name: String
